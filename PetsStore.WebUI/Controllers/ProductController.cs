28dd340d78804c93f244d969016324d7ab4f7cf2
dochirwa <chidor@dunwoody.edu>
2024-04-17 21:51:37 -0500

    Add project files.

---
+++ PetsStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using PetsStore.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PetsStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository myrepository;
+
+        public ProductController (IProductRepository prodRepo)
+        {
+            this.myrepository = prodRepo;
+        }
+
+        public ViewResult List()
+        {
+            return View(myrepository.Products);
+        }
+    }
+}
---
+++ PetsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+using Moq;
+using Ninject;
+using System;
+using System.Web.Mvc;
+using System.Collections.Generic;
+using PetsStore.Domain.Abstract;
+using PetsStore.Domain.Entities;
+using PetsStore.Domain.Concrete;
+
+
+namespace PetsStore.WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel mykernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            mykernel = kernelParam;
+            AddBindings();
+        }
+
+        public object GetService(Type myserviceType)
+        {
+            return mykernel.TryGet(myserviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type myserviceType)
+        {
+            return mykernel.GetAll(myserviceType);
+        }
+
+        private void AddBindings()
+        {
+            /*Mock<IProductRepository> myMock = new Mock<IProductRepository>();
+
+            myMock.Setup(m => m.Products).Returns(new List<Product> {
+                 new Product { Name = "Bird Seed", Price = 25},
+                 new Product { Name = "Chew Toy", Price = 179},
+                 new Product { Name = "Training Treats", Price = 95},
+                 });*/
+            mykernel.Bind<IProductRepository>().To<EFProductRepository>();
+        }
+    }
+}
